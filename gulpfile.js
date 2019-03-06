f45648b7c5495b04cb4b9fba2b90ad6362057c84
fishyberry <spambaqfish+berry@gmail.com>
2019-03-06 22:21:26 +0100

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,44 @@
-// Load plugins
-const browsersync = require("browser-sync").create();
-const gulp = require("gulp");
+/**
+ * Created by pharaoh on 18/12/2017.
+ */
 
-// Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function(cb) {
+'use strict';
+const gulp = require('gulp');
+const sass = require("gulp-ruby-sass");
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const livereload = require("gulp-livereload");
 
-  // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
 
-  // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'))
 
-  cb();
-
+gulp.task('style',function () {
+    sass('assets/scss/**/*.scss',{ style: 'expanded' })
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(gulp.dest('assets/css/'))
+        .pipe(livereload());
 });
 
-// BrowserSync
-function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-  done();
-}
+//minify all js to one js file
+gulp.task('minscripts', function() {
+    return gulp.src(['assets/js/jquery-migrate.js', 'assets/js/jquery.waypoints.min.js','assets/js/popper.js', 'assets/js/bootstrap.min.js','assets/js/wow.min.js','assets/js/bootstrap-progressbar.min.js', 'assets/js/jquery.counterup.min.js','assets/js/slick.min.js', 'assets/js/jquery.magnific-popup.min.js' ,'assets/js/jquery.filterizr.min.js'])
+        .pipe(concat('plugins.js'))
+        .pipe(gulp.dest('assets/'));
+});
 
-// BrowserSync Reload
-function browserSyncReload(done) {
-  browsersync.reload();
-  done();
-}
+//minify all css to one css file
 
-// Watch files
-function watchFiles() {
-  gulp.watch("./**/*.html", browserSyncReload);
-}
+gulp.task('mincss',function () {
+    return gulp.src(['assets/css/bootstrap.min.css','assets/css/animate.min.css','assets/css/et-line.css','assets/css/font-awesome.min.css','assets/css/magnific-popup.css','assets/css/slick.css','assets/css/ionicons.min.css'])
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest('assets/'))
+});
 
-gulp.task("default", gulp.parallel('vendor'));
 
-// dev task
-gulp.task("dev", gulp.parallel(watchFiles, browserSync));
+gulp.task('watch',function () {
+    livereload.listen();
+    gulp.watch('assets/scss/**/*.scss',['style'])
+});
+
+gulp.task('default',['style','watch']);
